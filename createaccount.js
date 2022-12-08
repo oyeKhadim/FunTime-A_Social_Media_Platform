2dd1b20c770909e0f7d504beff21ca166538569e
Abdul Mateen <98536971+notrealmateen@users.noreply.github.com>
2022-12-08 22:52:24 +0500

    Changed methods to static and added methods

--- createaccount.js
+++ createaccount.js
@@ -1,18 +1,22 @@
 import { Data1 } from './DataLayer.js'
-Data1.persons['2']=2;
 let signup = document.getElementById('signup');
 signup.onclick = (() => {
     let user = document.getElementById('username').value;
     let pass = document.getElementById('password').value;
     let repass = document.getElementById('repassword').value;
     if (pass == repass) {
-        if (!Data1.checkUserExist(user)) {
-            // open page
-            alert('succesful')
-            // D.persons[user]=pass;
+        if (Data1.usernameValid(user)) {
+            if (!Data1.checkUserExist(user)) {
+                alert('succesful')
+                Data1.persons[user] = pass;           // Adds it to the map
+                // open page
+            }
+            else {
+                alert('Username already Taken.')
+            }
         }
         else {
-            alert('User already Exists')
+            alert('Username is not valid. There must be no spaces.')
         }
     }
     else {
