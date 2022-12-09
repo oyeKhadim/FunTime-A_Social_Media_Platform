f8badce9e565367168e31cd2462dbe2f3ac0bca9
Khadim92 <imkhadim1@gmail.com>
2022-12-09 07:31:57 +0500

    when user signed up its data is stored in map successfully

--- createaccount.js
+++ createaccount.js
@@ -8,17 +8,19 @@ signup.onclick = (() => {
     if (pass == repass) {
         if (Data1.usernameValid(user)) {
             if (!Data1.checkUserExist(user)) {
-                alert('succesful')
-                var per = new person();
-                per.password = pass;
-                per.username = user;
-                Data1.persons[user] = pass;           // Adds it to the map          //Make sure this is working and if it is not working make it happen
-                console.log(Data1.persons[user].password)
-                // open page
-            }
-            else {
-                alert('Username already Taken.')
-            }
+							alert("succesful");
+                            Data1.persons.set(user,pass) // <--It is working now
+							// var per = new person();
+							// per.password = pass;
+							// per.username = user;
+                            // Data1.arr=[2,4]
+                            // console.log(Data1.arr);
+							// Data1.persons[user] = pass; // Adds it to the map          //Make sure this is working and if it is not working make it happen
+							console.log(Data1.persons);
+							// open page
+						} else {
+							alert("Username already Taken.");
+						}
         }
         else {
             alert('Username is not valid. There must be no spaces.')
