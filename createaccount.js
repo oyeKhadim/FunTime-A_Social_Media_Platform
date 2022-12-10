3e6fee8bb673ae2d0bd5624180dd6abd46136f50
Khadim92 <imkhadim1@gmail.com>
2022-12-10 10:03:36 +0500

    Did many changes

--- createaccount.js
+++ createaccount.js
@@ -1,43 +1,119 @@
-import { Data1 } from './DataLayer.js'
-import { person } from './personClass.js'
-let signup = document.getElementById('signup');
-signup.onclick = (() => {
-    let user = document.getElementById('username').value;
-    let pass = document.getElementById('password').value;
-    let repass = document.getElementById('repassword').value;
-    if (pass == repass) {
-        if (Data1.usernameValid(user)) {
-            if (!Data1.checkUserExist(user)) {
-                alert("succesful");
-                Data1.persons.set(user, pass)   // <--It is working now
-                console.log(Data1.persons);
-                // open page
-            } else {
-                alert("Username already Taken.");
-            }
-        }
-        else {
-            alert('Username is not valid. There must be no spaces.')
-        }
-    }
-    else {
-        alert('Password does not match.')
-    }
-})
-let showpassword = document.getElementById("showpassword")
-let showpass = document.getElementById("showrepassword")
+import { Data1 } from "./DataLayer.js";
+import { person } from "./personClass.js";
+// let signup = document.getElementById('signup');
+// signup.onclick = (() => {
+//     let user = document.getElementById('username').value;
+//     let pass = document.getElementById('password').value;
+//     let repass = document.getElementById('repassword').value;
+//     if (pass == repass) {
+//         if (Data1.usernameValid(user)) {
+//             if (!Data1.checkUserExist(user)) {
+//                 alert("succesful");
+//                 Data1.persons.set(user, pass)   // <--It is working now
+//                 console.log(Data1.persons);
+//                 // open page
+//             } else {
+//                 alert("Username already Taken.");
+//             }
+//         }
+//         else {
+//             alert('Username is not valid. There must be no spaces.')
+//         }
+//     }
+//     else {
+//         alert('Password does not match.')
+//     }
+// })
+let showpassword = document.getElementById("showpassword");
+let showpass = document.getElementById("showrepassword");
 showpassword.onclick = () => {
-    if (document.getElementById("password").type == 'text') {
-        document.getElementById("password").type = "password";
-        document.getElementById("repassword").type = "password";
-        showpassword.src = "./closedeye.png";
-        showpass.src = "./closedeye.png";
+	if (document.getElementById("password").type == "text") {
+		document.getElementById("password").type = "password";
+		document.getElementById("repassword").type = "password";
+		showpassword.src = "../Pngs/closedeye.png";
 
-        return;
-    }
-    document.getElementById("password").type = "text";
-    showpassword.src = "./openeye.png";
-    document.getElementById("repassword").type = "text";
-    showpass.src = "./openeye.png";
+		return;
+	}
+	document.getElementById("password").type = "text";
+	showpassword.src = "../Pngs/openeye.png";
+	document.getElementById("repassword").type = "text";
+};
 
+//<--------------------------------Bachy is code se door rahy--------------------------------------->
+
+//<------------------------------------Linking With DataBase---------------------------------------->
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-analytics.js";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyDgjxC3pegDxwodMo_tBF6og-5NAtccu_U",
+	authDomain: "justteatingfirebase.firebaseapp.com",
+	databaseURL: "https://justteatingfirebase-default-rtdb.firebaseio.com",
+	projectId: "justteatingfirebase",
+	storageBucket: "justteatingfirebase.appspot.com",
+	messagingSenderId: "1055889204987",
+	appId: "1:1055889204987:web:33450c37ba9cf8d798b25e",
+	measurementId: "G-LQLCQBVPS3",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+import {
+	getDatabase,
+	get,
+	set,
+	ref,
+	child,
+	update,
+	remove,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+const db = getDatabase();
+
+let signup = document.getElementById("signup");
+signup.addEventListener("click", insertData);
+function insertData() {
+	const dbref = ref(db);
+	let user = document.getElementById("username").value;
+    let flag=true;
+	get(child(dbref, "Usernames/" + user)).then((snapshot) => {
+		if (snapshot.exists()) {
+			console.log("true");
+			
+		} else {
+			console.log("false");
+			flag= false;
+		}
+	});
+	let pass = document.getElementById("password").value;
+	let repass = document.getElementById("repassword").value;
+	console.log("function callled");
+    setTimeout(()=>{if (pass == repass) {
+			if (Data1.usernameValid(user)) {
+				if (flag) {
+					alert("Username already Taken.");
+				} else {
+					set(ref(db, "Usernames/" + username.value), {
+						username: username.value,
+						password: password.value,
+					})
+						.then(() => {
+							alert("Data stored suxxessfully");
+						})
+						.catch((error) => {
+							alert("Error: " + error);
+						});
+					// open page
+				}
+			} else {
+				alert("Username is not valid. There must be no spaces.");
+			}
+		} else {
+			alert("Password does not match.");
+		}},1000)
+	
 }
+
+// <-------------------------------------------Linking Ends Here------------------------------------------>
--- loginpage.js
+++ loginpage.js
@@ -1,29 +1,90 @@
-import { Data1 } from './DataLayer.js'
-let v = document.getElementById('signinbtn');
-v.onclick = (() => {
-    let user = document.getElementById('username').value;
-    let pass = document.getElementById('password').value;
-    if (Data1.checkUser(user, pass)) {
-        alert('user found')
-        // open page
-    }
-    else {
-        document.getElementById('username').value = '';
-        document.getElementById('password').value = '';
-        alert('No such User')
-        //when page refreshes or we swithch to another page all data lost
-        console.log(Data1.arr);
-        
-    }
-})
-let showpassword=document.getElementById("showpassword")
-showpassword.onclick=()=>{
-    if(document.getElementById("password").type=='text'){
-        document.getElementById("password").type = "password";
-        showpassword.src="./closedeye.png";
-        return;
-    }
-    document.getElementById("password").type = "text";
-        showpassword.src = "./openeye.png";
+import { Data1 } from "./DataLayer.js";
+let v = document.getElementById("signinbtn");
+// v.onclick = () => {
+// 	let user = document.getElementById("username").value;
+// 	let pass = document.getElementById("password").value;
+// 	if (Data1.checkUser(user, pass)) {
+// 		alert("user found");
+// 		// open page
+// 	} else {
+// 		document.getElementById("username").value = "";
+// 		document.getElementById("password").value = "";
+// 		alert("No such User");
+// 		//when page refreshes or we swithch to another page all data lost
+// 		console.log(Data1.arr);
+// 	}
+// };
+let showpassword = document.getElementById("showpassword");
+showpassword.onclick = () => {
+	if (document.getElementById("password").type == "text") {
+		document.getElementById("password").type = "password";
+		showpassword.src = "../Pngs/closedeye.png";
+		return;
+	}
+	document.getElementById("password").type = "text";
+	showpassword.src = "../Pngs/openeye.png";
+};
 
+
+
+
+//<--------------------------------Bachy is code se door rahy--------------------------------------->
+
+//<------------------------------------Linking With DataBase---------------------------------------->
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-analytics.js";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyDgjxC3pegDxwodMo_tBF6og-5NAtccu_U",
+	authDomain: "justteatingfirebase.firebaseapp.com",
+	databaseURL: "https://justteatingfirebase-default-rtdb.firebaseio.com",
+	projectId: "justteatingfirebase",
+	storageBucket: "justteatingfirebase.appspot.com",
+	messagingSenderId: "1055889204987",
+	appId: "1:1055889204987:web:33450c37ba9cf8d798b25e",
+	measurementId: "G-LQLCQBVPS3",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+import {
+	getDatabase,
+	get,
+	set,
+	ref,
+	child,
+	update,
+	remove,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+const db = getDatabase();
+
+var username = document.getElementById("username");
+var password = document.getElementById("password");
+
+
+
+function SelectData() {
+	const dbref = ref(db);
+	get(child(dbref, "Usernames/" + username.value)).then((snapshot) => {
+		if (snapshot.exists()) {
+			if(username.value == snapshot.val().username&&
+			password.value == snapshot.val().password){
+                alert('Valid User');
+            }
+            else{
+                alert('wrong password');
+            }
+		} else {
+			alert("No Data Found");
+		}
+	});
+    
 }
+
+var signinbtn = document.getElementById("signinbtn");
+signinbtn.addEventListener("click", SelectData);
+
+// <-------------------------------------------Linking Ends Here------------------------------------------>
