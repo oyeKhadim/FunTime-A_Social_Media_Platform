4b455851111034bb4df8db935552e4de93721b93
Abdul Mateen <98536971+notrealmateen@users.noreply.github.com>
2022-12-10 18:48:44 +0500

    error fixed

--- loginpage.js
+++ loginpage.js
@@ -1,19 +1,3 @@
-import { Data1 } from "./DataLayer.js";
-let v = document.getElementById("signinbtn");
-// v.onclick = () => {
-// 	let user = document.getElementById("username").value;
-// 	let pass = document.getElementById("password").value;
-// 	if (Data1.checkUser(user, pass)) {
-// 		alert("user found");
-// 		// open page
-// 	} else {
-// 		document.getElementById("username").value = "";
-// 		document.getElementById("password").value = "";
-// 		alert("No such User");
-// 		//when page refreshes or we swithch to another page all data lost
-// 		console.log(Data1.arr);
-// 	}
-// };
 let showpassword = document.getElementById("showpassword");
 showpassword.onclick = () => {
 	if (document.getElementById("password").type == "text") {
@@ -24,9 +8,6 @@ showpassword.onclick = () => {
 	document.getElementById("password").type = "text";
 	showpassword.src = "../Pngs/openeye.png";
 };
-
-
-
 
 //<--------------------------------Bachy is code se door rahy--------------------------------------->
 
